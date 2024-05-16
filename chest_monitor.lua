aef46d05759f269b9f5c5a6661f0226aae2fd216
bayoneerhart758@gmail.com <BayonEerhart>
2024-05-16 11:41:21 +0200

    done

--- chest_monitor.lua
+++ chest_monitor.lua
@@ -2,17 +2,18 @@
 function print_inventory_on_monitor(chest, monitor)
     local cursor = 5
     for slot, item in pairs(chest.list()) do
+        
         cursor = cursor + 1
         local itemName = string.gsub(item.name, "minecraft:", "") 
         local cursoritem = math.ceil(string.len(itemName) / 2) 
         monitor.setCursorPos(25 - cursoritem, cursor) 
         monitor.setTextColour(1)
         monitor.write(itemName)
-        monitor.setTextColour(256)
         cursor = cursor + 1
         local itemcount = string.gsub(item.count, "minecraft:", "") 
         local cursoramount = math.ceil(string.len(itemcount) / 2) 
         monitor.setCursorPos(24 - cursoramount, cursor)
+        monitor.setTextColour(256)
         monitor.write(tostring(itemcount).. "x")
     end
 end
